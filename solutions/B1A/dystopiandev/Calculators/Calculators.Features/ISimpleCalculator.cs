ef2211b73eea24b232e66b90d9e3b3cd16275271
Redhart Azul <me@dystopian.dev>
2023-04-11 18:54:21 +0100

    update docs; add sample solution

---
+++ solutions/B1A/dystopiandev/Calculators/Calculators.Features/ISimpleCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Calculators.Features;
+
+public interface ISimpleCalculator<T> where T : INumber<T>
+{
+    T Add(T a, T b);
+    T Subtract(T a, T b);
+    T Multiply(T a, T b);
+    T Divide(T a, T b);
+}
---
+++ solutions/B1A/dystopiandev/Calculators/Calculators.Features/SimpleCalculator.cs
@@ -0,0 +1,26 @@
+using System.Numerics;
+
+namespace Calculators.Features;
+
+public class SimpleCalculator<T> : ISimpleCalculator<T> where T : INumber<T>
+{
+    public T Add(T a, T b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Subtract(T a, T b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Multiply(T a, T b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T Divide(T a, T b)
+    {
+        throw new NotImplementedException();
+    }
+}
