1c865ddce1ed4bd9dccb794cd89a6aede87772ef
ricanteq <ricanteq@gmail.com>
2023-04-12 19:31:34 +0100

    initial

---
+++ solutions/B1A/ricanteq/Calculators/Calculators.Features/ISimpleCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+namespace Calculators.Features;
+
+public interface ISimpleCalculator<T>
+    where T : struct, IComparable, IConvertible, IEquatable<T>, IFormattable, INumberBase<T>
+{
+    T Add(T a, T b);
+    T Subtract(T a, T b);
+    T Multiply(T a, T b);
+    T Divide(T a, T b);
+}
